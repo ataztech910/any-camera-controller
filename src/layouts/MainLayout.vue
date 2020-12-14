55676c01083156d273834bff3403ae73713b83ff
Andrei <webconsult.ekb@gmail.com>
2020-12-14 23:18:09 +0100

    app.ication is ready for test

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,21 +1,5 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
     <q-page-container>
       <router-view />
     </q-page-container>
